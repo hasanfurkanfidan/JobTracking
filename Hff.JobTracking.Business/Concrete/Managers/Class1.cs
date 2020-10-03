a63c7fdd46c95450f8a0d05d8ec9d8ac900eef88
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-10-03 23:51:59 +0300

    class1 added

---
+++ Hff.JobTracking.Business/Concrete/Managers/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hff.JobTracking.Business.Concrete.Managers
+{
+    class Class1
+    {
+    }
+}
