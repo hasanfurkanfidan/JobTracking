47a36e58c9e5f63950dbad905db60968555660b8
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-10-04 13:36:02 +0300

    Added Urgency Implementation In Business

---
+++ Hff.JobTracking.Business/Abstract/IUrgencyService.cs
@@ -0,0 +1,16 @@
+﻿using Hff.JobTracking.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hff.JobTracking.Business.Abstract
+{
+    public interface IUrgencyService
+    {
+        List<Urgency> GetUrgencies();
+
+        
+    }
+}
---
+++ Hff.JobTracking.Business/Concrete/Managers/UrgencyManager.cs
@@ -0,0 +1,24 @@
+﻿using Hff.JobTracking.Business.Abstract;
+using Hff.JobTracking.DataAccess.Abstract;
+using Hff.JobTracking.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hff.JobTracking.Business.Concrete.Managers
+{
+    public class UrgencyManager : IUrgencyService
+    {
+        private readonly IUrgencyDal _urgencyDal;
+        public UrgencyManager(IUrgencyDal urgencyDal)
+        {
+            _urgencyDal = urgencyDal;
+        }
+        public List<Urgency> GetUrgencies()
+        {
+            return _urgencyDal.GetList();
+        }
+    }
+}
